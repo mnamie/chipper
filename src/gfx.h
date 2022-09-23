256c023ad639fc8918e42aa964d95462a869dda8
Michael <mnamie20@gmail.com>
2022-09-23 17:43:23 -0400

    more

--- src/gfx.h
+++ src/gfx.h
@@ -8,13 +8,6 @@
 #define WINDOW_HEIGHT 640
 #define WINDOW_WIDTH 1280
 
-// Required globals for rendering with SDL2
-// typedef struct Chip8IO {
-//     SDL_Window* screen;
-//     SDL_Renderer* renderer;
-//     SDL_Texture* texture;
-// } Chip8IO;
-
 void init_display();
 void buffer_draw(Chip8* system);
 void draw(Chip8* system);
